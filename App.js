6a01d136bd6db32718c99a9cac45d30b79c76b15
jtirkkon <jouni.tirkkonen@gmail.com>
2021-01-29 23:15:02 +0200

    Valmis

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
+  const [product, setProduct] = useState('');
+  const [shoppingList, setShoppingList] = useState([]);
+
+  const addProduct = () => {
+    setShoppingList([...shoppingList, {key: product}]);
+    setProduct('');
+  }
+
+  const clearList = () => {
+    setShoppingList([]);
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput style={styles.textInputStyle} onChangeText={text => setProduct(text)} value={product}></TextInput>
+      <View style={styles.buttonContainer}>
+        <View style={{marginRight: 30}}>
+          <Button title="ADD" onPress={addProduct}></Button>
+        </View>
+        <Button title="CLEAR" onPress={clearList}></Button>
+      </View>
+
+      <Text style={styles.shoppingListStyle}>Shopping List</Text>
+      <FlatList data={shoppingList} renderItem={({item}) => <Text>{item.key}</Text>} />
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +39,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 100
   },
+  textInputStyle: {
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+    marginBottom: 30
+  },
+  shoppingListStyle: {
+    fontWeight: 'bold',
+    color: '#00008B'  
+  }
 });
